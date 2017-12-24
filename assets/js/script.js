3d38d85db04b1966fe5293b13952e6cda9157fe0
malcodeman <malcodeman@users.noreply.github.com>
2017-12-25 00:20:03 +0100

    Basic pull and render

---
+++ assets/js/script.js
@@ -0,0 +1,63 @@
+var Xray = require('x-ray');
+var x = Xray();
+
+x('https://www.klix.ba/najnovije', 'article', [{
+        headline: 'a h1',
+        link: "a@href",
+        category: ".above .kategorija",
+        shares: ".above .shareovi",
+        comments: ".below .comments"
+    }])
+    (function (err, res) {
+        for (let i = 0; i < res.length; ++i) {
+            renderArticle(i + 1, res[i].headline, res[i].shares, res[i].comments);
+        }
+    })
+
+
+function renderArticle(id, headline, shares, comments) {
+    let article = document.createElement("article");
+    article.classList.add("article");
+
+    let article_id = document.createElement("span");
+    article_id.classList.add("article-id");
+    article_id.textContent = id;
+
+    let article_title = document.createElement("span");
+    article_title.classList.add("article-title");
+    article_title.textContent = headline;
+
+    let div1 = document.createElement("div");
+
+    div1.appendChild(article_id);
+    div1.appendChild(article_title);
+
+    let article_shares = document.createElement("span");
+    article_shares.classList.add("article-shares");
+    if (shares == 1) {
+        console.log("Usao !!")
+        shares += " share"
+    } else {
+        shares += " shares"
+    }
+    article_shares.textContent = shares;
+
+    let article_comments = document.createElement("span");
+    article_comments.classList.add("article-comments");
+    if (comments == 1) {
+        comments += " comment"
+    } else {
+        comments += " comments"
+    }
+    article_comments.textContent = comments;
+
+    let div2 = document.createElement("div");
+
+    div2.appendChild(article_shares);
+    div2.appendChild(article_comments);
+
+    article.appendChild(div1);
+    article.appendChild(div2);
+
+    document.getElementById("main").appendChild(article);
+}
