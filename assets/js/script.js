01c64fc45c1b44d7ee28ea405da25dca95f70abc
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2018-10-18 20:28:37 +0200

    Total refactor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,75 +1,49 @@
-const fs = require("fs");
-
-const renderArticles = require("./assets/js/renderArticles");
-const sortArticles = require("./assets/js/sortArticles");
-const ls = require("./assets/js/localStorage");
-//const getArticles = require("./assets/js/getArticles");
 const windowControls = require("./assets/js/windowControls");
-const osmo = require("./assets/js/osmo");
 
-function loadArticles() {
-    return JSON.parse(fs.readFileSync("response.json", "utf8"));
-}
+const Article = ({ id, url, lead, shares, comments }) => `
+<a class="article" href="${url}">
+    <div>
+        <span class="article-id">${id}</span>
+        <span class="article-title">${lead}</span>
+    </div>
+    <div>
+        <span class="article-shares">${shares} shares</span>
+        <span class="article-comments">${comments} comments</span>
+    </div>
+</a>
+`;
 
-function changeTextContent(elementId, newText) {
-    document.getElementById(elementId).textContent = newText;
-}
-
-function changeSort(art) {
-    clearElement("main");
-    let sort = ls.getFromLocalStorage("sort");
-    if (sort === "date") {
-        renderArticles.renderArticles(sortArticles.sortByComments(art, true));
-        changeTextContent("sort_label", "Comments");
-        ls.setToLocalStorage("sort", "comments");
-    } else if (sort === "comments") {
-        renderArticles.renderArticles(sortArticles.sortByShares(art, true));
-        changeTextContent("sort_label", "Shares");
-        ls.setToLocalStorage("sort", "shares");
-    } else if (sort === "shares") {
-        renderArticles.renderArticles(loadArticles());
-        changeTextContent("sort_label", "Date");
-        ls.setToLocalStorage("sort", "date");
-    }
-}
-
-function clearElement(elementId) {
-    document.getElementById(elementId).innerHTML = "";
-}
-
-function formatArticles(articles) {
-    articles.forEach(article => {
-        if (article.shares.includes("k")) {
-            article.shares = article.shares.replace("k", "");
-            article.shares *= 1000;
-        }
+function getLatest() {
+  const url = "https://micro-klix.herokuapp.com/latest";
+  return fetch(url)
+    .then(response => response.json())
+    .then(articles => {
+      return articles;
+    })
+    .catch(error => {
+      console.log(error);
     });
-    return articles;
 }
 
-function main() {
-    clearElement("main");
-    for (let i = 1; i < 33; i++) {
-        let temp = osmo.get(i);
-        console.log(temp)
-        temp.then((res) => {
-            res = formatArticles(res);
-            renderArticles.renderArticles(res);
-        });
-    }
-    ls.clearLocalStorage();
-    ls.setToLocalStorage("sort", "date");
+async function main() {
+  const main = document.getElementById("main");
+  const articles = await getLatest();
+  articles.map((article, index) => {
+    main.insertAdjacentHTML(
+      "beforeend",
+      Article({
+        id: index,
+        url: article.url,
+        lead: article.lead,
+        shares: article.shares,
+        comments: article.comments
+      })
+    );
+  });
 }
 
 document.getElementById("x").addEventListener("click", () => {
-    windowControls.close();
+  windowControls.close();
 });
-document.getElementById("sort").addEventListener("click", () => {
-    changeSort(art);
-});
-/*document.getElementById("new").addEventListener("click", () => {
-    clearMain();
-    getArticles(1);
-});*/
 
 main();
--- assets/js/windowControls.js
+++ assets/js/windowControls.js
@@ -1,21 +1,19 @@
-const {
-    remote
-} = require("electron");
+const { remote } = require("electron");
 
 function minimize() {
-    remote.BrowserWindow.getFocusedWindow().minimize();
+  remote.BrowserWindow.getFocusedWindow().minimize();
 }
 
 function maximize() {
-    remote.BrowserWindow.getFocusedWindow().maximize();
+  remote.BrowserWindow.getFocusedWindow().maximize();
 }
 
 function close() {
-    remote.BrowserWindow.getFocusedWindow().close();
+  remote.BrowserWindow.getFocusedWindow().close();
 }
 
 module.exports = {
-    minimize: minimize,
-    maximize: maximize,
-    close: close
-}
+  minimize: minimize,
+  maximize: maximize,
+  close: close
+};
