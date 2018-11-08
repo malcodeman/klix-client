5da82f29e86e0b92098994aff87cca2ea2a3591f
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2018-11-08 14:21:48 +0100

    Adds article fetching

--- src/routes/articles/index.js
+++ src/routes/articles/index.js
@@ -1,14 +1,55 @@
-import { h } from "preact";
+import { render, Component } from "preact";
 import { Link } from "preact-router/match";
 
 import style from "./style";
 
-const Articles = () => (
-  <div>
-    <h1>Home</h1>
-    <p>This is the Home component.</p>
-    <Link href="/articles/test">Article test</Link>
-  </div>
-);
+const Article = props => {
+  const { id, lead, shares, comments } = props;
+  return (
+    <article class={style.article}>
+      <div>
+        <span class={style.id}>{id}</span>
+        <span class={style.lead}>{lead}</span>
+      </div>
+      <div>
+        <span class={style.shares}>{shares} shares</span>
+        <span>{comments} comments</span>
+      </div>
+    </article>
+  );
+};
+
+class Articles extends Component {
+  state = {
+    articles: null
+  };
+  componentDidMount() {
+    this.getPage(1)
+  }
+  async getPage = page => {
+    const url = `https://micro-klix.herokuapp.com/pages/${page}`;
+    const articles = await fetch(url).then(response => response.json());
+    this.setState({ articles });
+  }
+  render() {
+    const { articles } = this.state;
+
+    return (
+      <div>
+        {articles &&
+          articles.articles.map(article => {
+            return (
+              <Article
+                id={article.id}
+                lead={article.lead}
+                shares={article.shares}
+                comments={article.comments}
+              />
+            );
+          })}
+      </div>
+    );
+  }
+}
 
 export default Articles;
