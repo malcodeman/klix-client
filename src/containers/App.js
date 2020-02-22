a5a7f563e2e6738a0463616df11ee4d8fc558d83
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2020-02-23 00:26:55 +0100

    Improves App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,55 +6,49 @@ import Loader from "../components/loader/Loader";
 
 const API_URL = process.env.REACT_APP_API_URL;
 
-class App extends React.Component {
-  state = {
-    posts: [],
-    fetching: false,
-    currentPage: 1
-  };
+function App() {
+  const [posts, setPosts] = React.useState([]);
+  const [fetching, setFetching] = React.useState(false);
 
-  componentDidMount = () => {
-    this.getLatest();
-  };
+  React.useEffect(() => {
+    getLatest();
+  }, []);
 
-  async getLatest() {
-    this.setState({ fetching: true });
+  async function getLatest() {
+    setFetching(true);
 
     const data = await fetch(`${API_URL}/latest`);
     const json = await data.json();
 
-    this.setState({ posts: json.articles, fetching: false });
+    setPosts(json.articles);
+    setFetching(false);
   }
 
-  render() {
-    const { posts, fetching } = this.state;
-
-    return (
-      <div className={style.app}>
-        <header className={style.header}>
-          <div className={style.headerContent}>Latest</div>
-        </header>
-        <main className={style.main}>
-          <div className={style.mainContent}>
-            {fetching && <Loader message={"Fetching posts..."} />}
-            {posts.map((post, index) => {
-              return (
-                <Post
-                  key={post.url}
-                  index={(index += 1)}
-                  comments={post.comments}
-                  headline={post.headline}
-                  lead={post.lead}
-                  shares={post.shares}
-                  url={post.url}
-                />
-              );
-            })}
-          </div>
-        </main>
-      </div>
-    );
-  }
+  return (
+    <div className={style.app}>
+      <header className={style.header}>
+        <div className={style.headerContent}>Latest</div>
+      </header>
+      <main className={style.main}>
+        <div className={style.mainContent}>
+          {fetching && <Loader message={"Fetching posts..."} />}
+          {posts.map((post, index) => {
+            return (
+              <Post
+                key={post.url}
+                index={(index += 1)}
+                comments={post.comments}
+                headline={post.headline}
+                lead={post.lead}
+                shares={post.shares}
+                url={post.url}
+              />
+            );
+          })}
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
